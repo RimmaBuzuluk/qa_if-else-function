8cda28ef6b0837f332040b92532932cdd6645fa4
Rymma-MAC <rymma-mac@MacBook-Air-Rymma-MAC.local>
2025-11-10 17:41:23 +0200

    add tests

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,30 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should condition return true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
 
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalled();
+    expect(first).toHaveBeenCalled();
+    expect(second).not.toHaveBeenCalled();
   });
 
-  // write tests here
+  it('should condition return false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalled();
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalled();
+  });
+
 });
